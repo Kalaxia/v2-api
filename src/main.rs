90c6252785c563ebc7aaf80b00cb67beeb2484f9
Axel Venet <kern046@gmail.com>
2020-04-12 18:57:44 +0200

    Setup Actix Web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/again")]
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(index2)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }

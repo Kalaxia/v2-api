c178101e7310a1b52715c49ed1e0d7410d152f2f
Axel Venet <kern046@gmail.com>
2020-04-16 18:21:27 +0200

    Move WebSocket logic to a dedicated module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
-use actix::{Actor, StreamHandler};
-use actix_web::{web, get, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web_actors::ws;
-
-/// Define http actor
-struct MyWs;
-
-impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod ws;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -35,19 +11,13 @@ async fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello world again!")
 }
 
-async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(MyWs {}, &req, stream);
-    println!("{:?}", resp);
-    resp
-}
-
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(index2)
-            .route("/ws/", web::get().to(ws_index))
+            .service(ws::server::ws_index)
     })
     .bind("127.0.0.1:8080")?
     .run()

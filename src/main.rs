f67a604e267de235a0da8d4fd27da0474b7e0127
Axel Venet <kern046@gmail.com>
2020-05-06 14:33:46 +0200

    Implement feature flag for ssl server mode

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use game::player;
 use std::collections::HashMap;
 use std::clone::Clone;
 use std::sync::RwLock;
+#[cfg(feature="ssl-secure")]
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 mod ws;
 mod game;
@@ -40,40 +42,56 @@ impl AppState {
     }
 }
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    // Start chat server actor
-    let state = web::Data::new(AppState {
+fn generate_state() -> AppState {
+    AppState {
         factions: RwLock::new(generate_factions()),
         lobbies: RwLock::new(HashMap::new()),
         players: RwLock::new(HashMap::new()),
-    });
+    }
+}
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(state.clone())
-            .service(
-                web::scope("/api")
-                .service(
-                    web::scope("/lobbies")
-                    .service(lobby::create_lobby)
-                    .service(lobby::get_lobbies)
-                    .service(lobby::get_lobby)
-                    .service(lobby::join_lobby)
-                    .service(lobby::leave_lobby)
-                )
-                .service(
-                    web::scope("/players")
-                    .service(player::get_current_player)
-                    .service(player::update_username)
-                    .service(player::update_faction)
-                    .service(player::update_ready)
-                )
-            )
-            .service(player::login)
-            .service(web::resource("/ws/").to(ws::client::entrypoint))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+// this function could be located in different module
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api")
+        .service(
+            web::scope("/lobbies")
+            .service(lobby::create_lobby)
+            .service(lobby::get_lobbies)
+            .service(lobby::get_lobby)
+            .service(lobby::join_lobby)
+            .service(lobby::leave_lobby)
+        )
+        .service(
+            web::scope("/players")
+            .service(player::get_current_player)
+            .service(player::update_username)
+            .service(player::update_faction)
+            .service(player::update_ready)
+        )
+    )
+    .service(player::login)
+    .service(web::resource("/ws/").to(ws::client::entrypoint));
+}
+
+#[actix_rt::main]
+#[cfg(target_feature="ssl-secure")]
+async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder.set_private_key_file("key.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
+    HttpServer::new(move || App::new().app_data(web::Data::new(generate_state())).configure(config))
+        .bind_openssl("127.0.0.1:8080", builder)?
+        .run()
+        .await
+}
+
+#[actix_rt::main]
+#[cfg(not(target_feature="ssl-secure"))]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().app_data(web::Data::new(generate_state())).configure(config))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }

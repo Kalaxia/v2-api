641477ec26b48fe523433a35afc24b0e7f9d8500
Axel Venet <kern046@gmail.com>
2020-05-07 12:26:02 +0200

    Implement default logger for server requests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use game::lobby;
 use game::player;
 use std::collections::HashMap;
 use std::clone::Clone;
 use std::sync::RwLock;
 use std::env;
+use env_logger;
 #[cfg(feature="ssl-secure")]
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
@@ -85,11 +87,16 @@ fn get_env(key: &str, default: &str) -> String {
 #[actix_rt::main]
 #[cfg(target_feature="ssl-secure")]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     builder.set_private_key_file(get_env("SSL_PRIVATE_KEY", "../var/ssl/key.pem").unwrap(), SslFiletype::PEM).unwrap();
     builder.set_certificate_chain_file(env::var_os("SSL_CERTIFICATE", "../var/ssl/cert.pem").unwrap()).unwrap();
 
-    HttpServer::new(move || App::new().app_data(web::Data::new(generate_state())).configure(config))
+    HttpServer::new(move || App::new()
+        .wrap(Logger::default())
+        .app_data(web::Data::new(generate_state())).configure(config))
         .bind_openssl(get_env("LISTENING_URL", "127.0.0.1:80"), builder)?
         .run()
         .await
@@ -98,7 +105,12 @@ async fn main() -> std::io::Result<()> {
 #[actix_rt::main]
 #[cfg(not(target_feature="ssl-secure"))]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().app_data(web::Data::new(generate_state())).configure(config))
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(move || App::new()
+        .wrap(Logger::default())
+        .app_data(web::Data::new(generate_state())).configure(config))
         .bind(get_env("LISTENING_URL", "127.0.0.1:80"))?
         .run()
         .await

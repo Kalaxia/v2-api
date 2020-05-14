1cec129b6b8769206b26cc6761451d4dab0770d3
Axel Venet <kern046@gmail.com>
2020-05-14 16:04:48 +0200

    Implement name and color for factions

--- src/game/faction.rs
+++ src/game/faction.rs
@@ -4,6 +4,15 @@ use std::collections::HashMap;
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Faction{
     pub id: FactionID,
+    name: FactionName,
+    color: (u8, u8, u8)
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum FactionName{
+    Kalankar,
+    Valkar,
+    Adranite
 }
 
 #[derive(Serialize, Deserialize, Copy, Clone, Hash, PartialEq, Eq)]
@@ -11,8 +20,20 @@ pub struct FactionID(u8);
 
 pub fn generate_factions() -> HashMap<FactionID, Faction> {
     let mut factions = HashMap::new();
-    factions.insert(FactionID(1), Faction{ id: FactionID(1) });
-    factions.insert(FactionID(2), Faction{ id: FactionID(2) });
-    factions.insert(FactionID(3), Faction{ id: FactionID(3) });
+    factions.insert(FactionID(1), Faction{
+        id: FactionID(1),
+        name: FactionName::Kalankar,
+        color: (0,255,255)
+    });
+    factions.insert(FactionID(2), Faction{
+        id: FactionID(2),
+        name: FactionName::Valkar,
+        color: (0,0,255)
+    });
+    factions.insert(FactionID(3), Faction{
+        id: FactionID(3),
+        name: FactionName::Adranite,
+        color: (255,0,0)
+    });
     factions
 }

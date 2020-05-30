4897a4c173d3880533e8f3ab005867c943a8947f
Axel Venet <kern046@gmail.com>
2020-05-30 16:48:21 +0200

    Fix combat losses for victorious defenders

--- src/game/fleet/combat.rs
+++ src/game/fleet/combat.rs
@@ -19,15 +19,15 @@ pub fn engage(attacker: &mut Fleet, mut defenders: &mut HashMap<FleetID, Fleet>)
         attacker.nb_ships -= nb_defense_ships;
         return true;
     }
-    attacker.nb_ships = 0;
     distribute_losses(&mut defenders, nb_defense_ships, attacker.nb_ships);
+    attacker.nb_ships = 0;
     false
 }
 
 fn distribute_losses(fleets: &mut HashMap<FleetID, Fleet>, total_ships: usize, losses: usize) {
-    fleets.retain(|_, f| {
+    fleets.retain(|_, mut f| {
         let percent: f64 = f.nb_ships as f64 / total_ships as f64;
-        f.nb_ships = (losses as f64 * percent).floor() as usize;
+        f.nb_ships -= (losses as f64 * percent).floor() as usize;
         f.nb_ships > 0
     });
 }

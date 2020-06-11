4322310148b57bf8b9e676950ff743bfe5fec708
Axel Venet <kern046@gmail.com>
2020-06-11 18:32:53 +0200

    WIP

--- src/game/faction.rs
+++ src/game/faction.rs
@@ -2,52 +2,60 @@ use actix_web::{get, web, HttpResponse};
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use crate::{
-    AppState
+    AppState,
+    lib::Result,
 };
-use sqlx::PgPool;
+use sqlx::{PgPool, postgres::PgRow};
+use sqlx_core::row::Row;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Faction{
     pub id: FactionID,
-    name: String,
-    color: (u8, u8, u8)
+    pub name: String,
+    pub color: FactionColor,
 }
 
 #[derive(Serialize, Deserialize, Copy, Clone, Hash, PartialEq, Eq, Debug)]
 pub struct FactionID(pub u8);
+#[derive(Serialize, Deserialize, Copy, Clone, sqlx::Type)]
+#[sqlx(rename = "JSONB")]
+pub struct FactionColor{
+    pub r: i32,
+    pub g: i32,
+    pub b: i32,
+}
 
-pub fn generate_factions() -> HashMap<FactionID, Faction> {
-    let mut factions = HashMap::new();
-    factions.insert(FactionID(1), Faction{
-        id: FactionID(1),
-        name: String::from("Kalankar"),
-        color: (255,200,0)
-    });
-    factions.insert(FactionID(2), Faction{
-        id: FactionID(2),
-        name: String::from("Valkar"),
-        color: (0,0,255)
-    });
-    factions.insert(FactionID(3), Faction{
-        id: FactionID(3),
-        name: String::from("Adranite"),
-        color: (255,0,0)
-    });
-    factions
+impl From<FactionID> for i32 {
+    fn from (fid: FactionID) -> i32 { fid.0 as i32 }
+}
+
+impl Faction {
+    pub async fn find_all(db_pool: &PgPool) -> Vec<Self> {
+        let factions: Vec<Self> = sqlx::query("SELECT * FROM faction__factions ORDER BY id")
+            .map(Self::format)
+            .fetch_all(db_pool).await.expect("Could not retrieve factions");
+        factions
+    }
+
+    pub async fn find(fid: FactionID, db_pool: &PgPool) -> Option<Self> {
+        Some(sqlx::query("SELECT * FROM faction__factions WHERE id = ?")
+            .bind(i32::from(fid))
+            .map(Self::format)
+            .fetch_one(db_pool).await.expect("Could not retrieve faction"))
+    }
+
+    fn format(row: PgRow) -> Faction {
+        let id: i32 = row.get("id");
+    
+        Faction{
+            id: FactionID(id as u8),
+            name: row.get("name"),
+            color: row.get("color"),
+        }
+    }
 }
 
 #[get("/")]
-pub async fn get_factions(state: web::Data<AppState>, db: web::Data<PgPool>) -> Option<HttpResponse> {
-    let mut factions = state
-        .factions()
-        .iter()
-        .map(|(_, f)| f.clone())
-        .collect::<Vec<Faction>>()
-    ;
-    factions.sort_by(|f1, f2| {
-        let FactionID(fid1) = f1.id;
-        let FactionID(fid2) = f2.id;
-        fid1.cmp(&fid2)
-    });
-    Some(HttpResponse::Ok().json(factions))
+pub async fn get_factions(state: web::Data<AppState>, db_pool: web::Data<PgPool>) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(Faction::find_all(db_pool.get_ref()).await))
 }
